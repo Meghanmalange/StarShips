431ea2fb76bcb700e641a1f0e70f5636023efe73
meghanmalange <mmalange@studnets.kennesaw.edu>
2026-08-28 11:20:04 -0400

    Main Architecture

---
+++ build/CMakeFiles/RocketSimulator.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RocketSimulator.dir/src/main.cpp.obj"
+  "CMakeFiles/RocketSimulator.dir/src/main.cpp.obj.d"
+  "RocketSimulator.exe"
+  "RocketSimulator.exe.manifest"
+  "RocketSimulator.pdb"
+  "libRocketSimulator.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RocketSimulator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
