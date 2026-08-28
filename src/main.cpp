c41a124a4517122810692989289e496670280217
meghanmalange <mmalange@studnets.kennesaw.edu>
2026-08-28 11:30:14 -0400

    Add initial Rocket Simulator application

---
+++ src/main.cpp
@@ -0,0 +1,26 @@
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    sf::RenderWindow window(
+        sf::VideoMode({800, 800}),
+        "Rocket Simulator"
+    );
+
+    while (window.isOpen())
+    {
+        while (const std::optional event = window.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>())
+            {
+                window.close();
+            }
+        }
+
+        window.clear();
+
+        window.display();
+    }
+
+    return 0;
+}
